913abc47cec32bf1f26ef9e8e2318c54a93fe12d
Novaspacemodular <rabarikiran2375@gmail.com>
2026-08-01 17:57:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,3 +42,20 @@ sections.forEach(section => {
     section.style.transition = "0.8s ease";
     observer.observe(section);
 });
+const slides=document.querySelectorAll(".slide");
+
+let current=0;
+
+setInterval(()=>{
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current>=slides.length){
+current=0;
+}
+
+slides[current].classList.add("active");
+
+},3000);
